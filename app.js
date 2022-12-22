ce0f9a9d5ad14b319b3d5e5861211c5b9c22fad1
Alex Saavedra <fass28@icloud.com>
2022-12-21 22:14:28 -0500

    added public folder & styles v1

--- app.js
+++ app.js
@@ -3,51 +3,53 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+let items = ['buy food','cook food', 'eat food']
+let item = ''
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static(__dirname + '/public'))
+console.log('path',__dirname);
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
     let today = new Date()
     let currentDay = today.getDay()
     let day = ''
-    let dayEjs = ''
-
+    
     if (currentDay === 6 || currentDay === 5 ){
         day = 'weekend'
     } else {
         day = 'weekday'
     }
     console.log(currentDay);
-    switch (currentDay) {
-        case 1:
-            dayEjs = 'monday'
-            break;
-        case 2:
-            dayEjs = 'tuesday'
-            break;
-        case 3:
-            dayEjs = 'wendsday'
-            break;
-        case 4:
-            dayEjs = 'thursday'
-            break;
-        case 5:
-            dayEjs = 'friday'
-            break;
-        case 6:
-             dayEjs = 'saturday'
-             break;
-        case 0:
-            dayEjs = 'sunday'
-            break;
-        default:
-            console.log('error in getting the value' + currentDay);
+  
+    let options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
     }
+
+    let dayEjs = today.toLocaleDateString('en-US', options)
+    
     console.log(dayEjs);
-    res.render('list', {kindOfDay: day,
-        currentDayName: dayEjs})
+    res.render('list', {
+        kindOfDay: day,
+        currentDayName: dayEjs,
+        newlistItems: items
+    })
 })
 
+app.post('/', function(req, res){
+    item = req.body.item
+    items.push(item)
+    console.log(item);
+    console.log(items);
+    res.redirect('/')
+    
+})
 
 app.listen(3000, function(){
     console.log('server running on port 3000');
 })
+
