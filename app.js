1e501bc3253f1f4ba9478a927276f1189d997ec0
Alex Saavedra <fass28@icloud.com>
2022-12-28 15:56:19 -0500

    added DB to the blog

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* adding express & bodyParser */
 const express = require("express")
 const bodyParser = require("body-parser")
-const { getDate } = require('./date')
+const mongoose = require('mongoose')
 
 const app = express()
-let items = ['buy food','cook food', 'eat food']
-let item = ''
+/* let items = ['buy food','cook food', 'eat food']
+let item = '' */
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -13,14 +13,56 @@ app.use(express.static(__dirname + '/public'))
 console.log('path',__dirname);
 app.set('view engine', 'ejs')
 
+mongoose.set('strictQuery', true)
+mongoose.connect('mongodb://localhost:27017/todolistDB')
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: [true,'INSERTA EL NOMBRE DEl ITEM PNDJO']
+    }
+})
+
+
+const Item = mongoose.model('Item', itemSchema)
+
+const item1 = new Item({
+    name: 'welcome to your to DO list'
+})
+
+const item2 = new Item({
+    name: 'hit the button to add new item'
+})
+
+const item3 = new Item({
+    name: '<----  hit this to delete an item'
+})
+
+
+const defaultItems = [item1,item2,item3]
+/* 
+Item.insertMany(defaultItems, function(err){
+     if (err){
+         console.log(err)
+     } else {
+        console.log('succesfully saved all the list to todolistDB');
+    }
+ }) */
+
+
+
+
 app.get('/', function(req, res) {
-        let day = getDate()
-        
+    
+    Item.find({},function(err, items){
         res.render('list', {
-        currentDayName: day,
+        currentDayName: 'Today',
         newlistItems: items
+        })
     })
 })
+
+
 
 app.post('/', function(req, res){
     item = req.body.item
